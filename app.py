270042f2590017e9f3fa61591e9afaaf004923a1
Nolonetworks <58474493+Nolonetworks@users.noreply.github.com>
2020-05-11 18:12:16 -0400

    Ejercicio 2 - Listo

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import csv
 todos = []
 stop = False
 
@@ -7,18 +8,41 @@ def get_todos():
 
 def add_one_task(title):
     # your code here
+    todos.append(title)
+    print(todos)
+    return todos
     pass
 
 def print_list():
     global todos
+    for item in todos:
+        print(item)
     pass
 
 def delete_task(number_to_delete):
     # your code here
+    number_to_delete = int(number_to_delete) - 1
+    todos.pop(number_to_delete)
+    return todos
     pass
 
 def save_todos():
     # your code here
+    global todos
+    tareas = ""
+    i=0
+    for item in todos:
+        
+        if i == 0:
+            tareas = tareas + item
+            i = i + 1
+        else:
+            tareas = tareas + "," + item
+        print(tareas)
+
+    archivo  = open("tareas.csv","w+")
+    archivo.write(tareas)
+    archivo.close()
     pass
 
     
